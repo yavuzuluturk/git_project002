ba833954380fe01d2eb8742e44823017b606ad65
yavuzuluturk <yavuzuluturk@gmail.com>
2020-08-09 17:50:01 +0200

    the forth commit from the local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -7,6 +7,7 @@ public class FirstClass {
 		 System.out.println("This is the second commit from local master");
 		 System.out.println("This is the third commit from local master");
 	 	 System.out.println("This is the first commit from remote repo");
+	 	 System.out.println("This is the forth commit from local master");
 	}
 
 }
