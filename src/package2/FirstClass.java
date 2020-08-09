25072f5b1f061a43fd417a04f909062eedc85f70
yavuzuluturk <yavuzuluturk@gmail.com>
2020-08-09 17:12:30 +0200

    the secont commit from local repo

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		 System.out.println("This is my first commit from local master branch");
-
+		 System.out.println("This is the second commit from local master");
 	}
 
 }
